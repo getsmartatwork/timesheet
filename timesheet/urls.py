c36305e718b26252bc35a7ee22716e4d1e3f3a15
rajeshrapally <rajeshrapally1@gmail.com>
2021-10-31 15:23:36 +0530

    media static added

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from . import settings
 from django.urls import path,include
 
 urlpatterns = [
@@ -21,3 +23,4 @@ urlpatterns = [
     path('employee/',  include('employee.urls')),
     path('',  include('apidocument.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
